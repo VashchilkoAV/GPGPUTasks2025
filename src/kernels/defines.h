e6e2a4f0b4208ff00db6edcf5a55d7c450b8bdba
alexander <vashchilko.av@phystech.edu>
2025-10-26 23:39:27 +0800

    even bigger defines

--- src/kernels/defines.h
+++ src/kernels/defines.h
@@ -7,7 +7,7 @@
 
 #define RASSERT_ENABLED 0 // disabled by default, enable for debug by changing 0 to 1, disable before performance evaluation/profiling/commiting
 
-#define LOAD_K_VALUES_PER_ITEM 8
-#define LOAD_K_VALUES_PER_ITEM_REDUCTION 8
+#define LOAD_K_VALUES_PER_ITEM 16
+#define LOAD_K_VALUES_PER_ITEM_REDUCTION 16
 
 #endif // pragma once
